4ab927932c8499385ba740c8cf92b8c76febea81
J4X <y.buchrieser@gmail.com>
2023-04-21 23:01:18 +0200

    Improved the reader script

--- web3.js/reader.js
+++ web3.js/reader.js
@@ -1,18 +1,16 @@
-
 // Description:
 // A short script that lets you read out the storage of a smart contract
 
-// How to use:
-// 1. Change the RPC URL to the one of the network you're using
-// 2. Replace the first parameter of getStorageAt with the address of the contract you're targeting
-// 3. Change the second parameter to the storage slot you want to read out.
-
+// Fill in these values before running the script
+const RPC_URL = '<RPC_URL>'; // Replace with the RPC URL of the network you're using
+const CONTRACT_ADDRESS = '<CONTRACT_ADDRESS>'; // Replace with the address of the contract you're targeting
+const STORAGE_SLOT = '<STORAGE_SLOT>'; // Replace with the storage slot you want to read out
 
 // Import the web3.js library
 const Web3 = require('web3');
 
 // Set up the web3 provider using the RPC URL and chain ID of the custom blockchain
-const web3 = new Web3(new Web3.providers.HttpProvider('https://goerli.blockpi.network/v1/rpc/public'));
+const web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
 
 // Get the storage value at a specific address and position
 async function getStorageAt(address, position) {
@@ -24,5 +22,5 @@ async function getStorageAt(address, position) {
   }
 }
 
-// Call the getStorageAt function with a specific address and position
-getStorageAt('0xe43029d90B47Dd47611BAd91f24F87Bc9a03AEC2', 3);
+// Call the getStorageAt function with the specified contract address and storage slot
+getStorageAt(CONTRACT_ADDRESS, STORAGE_SLOT);
